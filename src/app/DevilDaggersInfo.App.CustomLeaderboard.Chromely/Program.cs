5e3a978cba4432a07b9c44ae2be6404b9079b0b5
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-10 00:58:13 +0200

    Attach Chromely app to DDCL UI

--- src/app/DevilDaggersInfo.App.CustomLeaderboard.Chromely/Program.cs
+++ src/app/DevilDaggersInfo.App.CustomLeaderboard.Chromely/Program.cs
@@ -1,8 +1,20 @@
 using Chromely.Core;
+using Chromely.Core.Configuration;
+using Chromely.Core.Infrastructure;
+using Chromely.Core.Network;
+using Chromely.NativeHosts;
 using DevilDaggersInfo.App.CustomLeaderboard.Chromely;
+
+IChromelyConfiguration config = DefaultConfiguration.CreateForRuntimePlatform();
+config.WindowOptions.Title = "Devil Daggers Custom Leaderboards";
+config.UrlSchemes.Add(new UrlScheme(DefaultSchemeName.LOCALREQUEST, "http", "app", string.Empty, UrlSchemeType.LocalResource, false));
+config.StartUrl = "http://app/index.html";
+
+ThreadApt.STA();
 
 AppBuilder
 	.Create(args)
+	.UseConfig<DefaultConfiguration>(config)
 	.UseApp<App>()
 	.Build()
 	.Run();
