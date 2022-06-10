3a35e873ceb989220da9cd34cade870659db074c
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-10 11:54:52 +0200

    Register services in Chromely

--- src/app/DevilDaggersInfo.App.CustomLeaderboard.Chromely/App.cs
+++ src/app/DevilDaggersInfo.App.CustomLeaderboard.Chromely/App.cs
@@ -1,5 +1,10 @@
 using Chromely;
+using DevilDaggersInfo.App.CustomLeaderboard.Chromely.Services;
+using DevilDaggersInfo.Core.CustomLeaderboard.Services;
+using DevilDaggersInfo.Core.NativeInterface;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DevilDaggersInfo.App.CustomLeaderboard.Chromely;
 
@@ -9,6 +14,18 @@ public class App : ChromelyBasicApp
 	{
 		base.ConfigureServices(services);
 
-		// TODO
+		IConfiguration configuration = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json")
+			.Build();
+
+		services.AddLogging(configure => configure.AddConsole());
+		services.AddSingleton<IClientConfiguration, ClientConfiguration>();
+		services.AddSingleton<IEncryptionService, EncryptionService>();
+		services.AddSingleton<INativeErrorReporter, NativeErrorReporter>();
+		services.AddSingleton<INativeMemoryService, NativeMemoryService>();
+		services.AddSingleton<NetworkService>();
+		services.AddSingleton<ReaderService>();
+		services.AddSingleton<UploadService>();
+		services.AddSingleton(configuration);
 	}
 }
--- src/app/DevilDaggersInfo.App.CustomLeaderboard.Wasm/Program.cs
+++ src/app/DevilDaggersInfo.App.CustomLeaderboard.Wasm/Program.cs
@@ -1,6 +1,3 @@
-using DevilDaggersInfo.App.CustomLeaderboard.NativeImpl.Windows;
-using DevilDaggersInfo.Core.CustomLeaderboard.Services;
-using DevilDaggersInfo.Core.NativeInterface;
 using DevilDaggersInfo.Razor.Core.CustomLeaderboard;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,18 +7,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-IConfiguration configuration = new ConfigurationBuilder()
-	//.AddJsonFile("appsettings.json")
-	.Build();
-
-builder.Services.AddSingleton<IClientConfiguration, ClientConfiguration>();
-builder.Services.AddSingleton<IEncryptionService, EncryptionService>();
-builder.Services.AddSingleton<INativeErrorReporter, NativeErrorReporter>();
-builder.Services.AddSingleton<INativeMemoryService, NativeMemoryService>();
-builder.Services.AddSingleton<NetworkService>();
-builder.Services.AddSingleton<ReaderService>();
-builder.Services.AddSingleton<UploadService>();
-builder.Services.AddSingleton(configuration);
 
 await builder.Build().RunAsync();
